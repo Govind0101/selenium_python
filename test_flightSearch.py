911757557c3bf771755556a7b40b29c23dcf84e4
VaishnaviSubramaniam <vaishnavi.solasa@gmail.com>
2023-11-01 21:41:27 -0400

    Added fixture ,description,  more assertions

--- test_flightSearch.py
+++ test_flightSearch.py
@@ -1,70 +1,77 @@
 import time
-import datetime
-
-import pytest as pytest
+import pytest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.common.keys import Keys
-
-# Custom variables Constants are in constants.py
-from constants import *
-
-# Get today's date in "day", "month" and "year"
-date = datetime.datetime.now()
-day = date.day
-month = date.month
-year = date.year
 
 
+# PyTest fixture to initialise the browser/driver
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
 
-class TestAutomation():
 
-    @pytest.fixture(autouse=True)
-    def launchBrowser(self):
-        driver = webdriver.Chrome()
-        self.driver.implicitly_wait(10)
-        yield
-        driver.close()
+def test_makemytrip_flight_search(driver):
+    driver.get("https://www.makemytrip.com/")
 
-    def test_Search_Flight(self):
-        try:
-            driver = self.driver
-            driver.get(URL)
+    time.sleep(2)
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//a[contains(@data-cy, 'LoginFlowPopup_97')]"))).click()
+    time.sleep(2)
+    driver.back()
+    time.sleep(2)
 
-            radioButton = driver.find_element_by_id(RADIO_BUTTON_ID)
-            radioButton.click()
-            fromField = driver.find_element_by_id(FROM_INPUT_ID)
-            fromField.send_keys(Keys.BACKSPACE)
-            fromField.send_keys(FROM_CITY)
-            toField = driver.find_element_by_id(TO_INPUT_ID)
-            toField.send_keys(Keys.BACKSPACE)
-            toField.send_keys(TO_CITY)
-            departDateField = driver.find_element_by_id(DEPART_DATE_ID)
-            departDateField.click()
-            driver.find_element_by_link_text("%s" % day).click()
-            returnDateField = driver.find_element_by_id(RETURN_DATE_ID)
-            returnDateField.click()
-            driver.find_element_by_link_text("%s" % (day + 1)).click()
-            search_button = driver.find_element_by_id(SEARCH_BUTTON_ID)
-            search_button.click()
-            time.sleep(5)
-            modal_dialog = driver.find_element_by_class_name(MODAL_DIALOG_CLASS)
+    # Wait for Flights element to be visible and click on it
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, "Flights"))).click()
 
-            if modal_dialog:
-                driver.find_element_by_link_text('Search Flight').click()
-            # time.sleep(3)
-            book_button_footer = WebDriverWait(driver, 20).until(
-                EC.element_to_be_clickable((By.CSS_SELECTOR, BOOK_BUTTON_FOOTER_CSS)))
-            book_button_footer.click()
+    time.sleep(3)
 
-            try:
-                confirm_modal_dialog = WebDriverWait(driver, 20).until(
-                    EC.element_to_be_clickable((By.ID, CONFIRM_MODAL_DIALOG_ID)))
-                driver.find_element_by_link_text('Continue to Book').click()
-            except:
-                pass
+    # Wait for ROUND TRIP option and click on it
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//li[text()='Round Trip']"))).click()
 
-        except Exception as e:
-            print(e)
+    time.sleep(3)
+
+    # Enter FROM Location as HYD
+    driver.find_element(By.ID, "fromCity").send_keys("HYD")
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//p[text()='Hyderabad, India']"))).click()
+
+    time.sleep(3)
+
+    # Enter TO Location as MAA
+    driver.find_element(By.ID, "toCity").send_keys("MAA")
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//p[text()='Chennai, India']"))).click()
+
+    time.sleep(3)
+
+    # Select DEPARTURE Date (27 August 2023)
+    driver.find_element(By.XPATH, "//span[text()='Departure']").click()
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//div[contains(@aria-label, 'Thu Sep 21 2023')]"))).click()
+
+    time.sleep(3)
+
+    # Select RETURN Date (31 August 2023)
+    driver.find_element(By.XPATH, "//span[text()='Return']").click()
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//div[contains(@aria-label, 'Sat Sep 23 2023')]"))).click()
+
+    time.sleep(3)
+
+    # Click on Search Button
+    driver.find_element(By.XPATH, "//a[text()='Search']").click()
+
+    time.sleep(15)
+
+    WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[text()= 'OKAY, GOT IT!']"))).click()
+
+    time.sleep(2)
+
+    # Validate the presence of flight details on the search results page
+    flight_details = driver.find_elements(By.XPATH, "//div[@class='listingCardWrap']")
+    assert len(flight_details) > 0, "flight details found on the search results page."
+    print("flight details found on the search results page.")
+    time.sleep(5)
